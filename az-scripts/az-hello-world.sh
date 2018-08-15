bcd38be1196c93417cc427b4918cd575b85dcbd0
SLathrop <steve@stela.io>
2018-08-15 16:08:33 -0400

    WIP scripts

--- az-scripts/az-hello-world.sh
+++ az-scripts/az-hello-world.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+#
+# Execute this directly in Azure Cloud Shell by pasting (SHIFT+INS on Windows, CTRL+V on Mac or Linux)
+# this command (uses shortened URL):
+#
+#   bash <(curl -sL https://git.io/slathrop-az-hello-world)
+#
 
 echo Hello World!
 echo Your Azure Subscriptions are as follows...
--- az-scripts/az-hello-you.sh
+++ az-scripts/az-hello-you.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
+#
+# Execute this directly in Azure Cloud Shell by pasting (SHIFT+INS on Windows, CTRL+V on Mac or Linux)
+# this command (uses shortened URL):
+#
+#   bash <(curl -sL https://git.io/slathrop-az-hello-you)
+#
 
 read -p "Enter your first name: " fname
+
 echo Hello $fname!
 echo Your Azure Subscriptions are as follows...
 az account list
