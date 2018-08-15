8bb9a6df14b0a0b82de58cb44f95438bb0babf8e
SLathrop <steve@stela.io>
2018-08-15 17:17:39 -0400

    WIP scripts

--- az-scripts/az-hello-arg.sh
+++ az-scripts/az-hello-arg.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 #
 # Execute this directly in Azure Cloud Shell (https://shell.azure.com) by pasting (SHIFT+INS on Windows, CTRL+V on Mac or Linux)
-# the following line (beginning with curl...) at the command prompt (uses shortened URL to this script as committed in GitHub repo):
+# the following line (beginning with curl...) at the command prompt (uses shortened URL to this script as committed in GitHub repo)
+# and then replacing the arg <YourFirstNameArg> with your First Name:
 #
 #   curl https://git.io/slathrop-az-hello-arg | bash -sL <YourFirstNameArg>
 #
