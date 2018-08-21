26f163156ef6bca4cc0a688f5ae876ffae4c01d8
SLathrop <steve@stela.io>
2018-08-21 12:17:58 -0400

    Uppercase bash var

--- az-scripts/az-hello-you.sh
+++ az-scripts/az-hello-you.sh
@@ -6,8 +6,8 @@
 #   bash <(curl -sL https://git.io/slathrop-az-hello-you)
 #
 
-read -p "Enter your first name: " fname
+read -p "Enter your first name: " FNAME
 
-echo Hello $fname!
+echo Hello $FNAME!
 echo Your Azure Subscriptions are as follows...
 az account list
